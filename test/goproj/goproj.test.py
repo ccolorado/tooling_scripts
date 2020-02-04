8e187e95fa9095c6cf47b32e8973873a475140d1
Carlos <carlos.colorado@gmail.com>
2020-02-04 10:54:25 -0600

    Adds goproj test playbook

---
+++ test/goproj/goproj.test.py
@@ -0,0 +1,39 @@
+import unittest
+
+class GoprojTest(unittest.TestCase):
+
+    # def test_is_valid_url_validates_path_exists():
+    # def test_is_valid_url_validates_path_is_directory():
+    # def test_key_exists_validates_key_exists():
+    # def test_save_stores_list_in_file():
+    # def test_save_stores_list_in_correct_order():
+    # def test_save_respect_commented_projects
+    # def test_read_project_list_matches_source_file
+    # def test_read_project_list_respects_commented_projects
+    # def test_get_project_list_contains_project_number():
+    # def test_get_project_list_contains_project_key():
+    # def test_get_project_list_contains_project_url():
+    # def test_print_list_matches_expected_output():
+    #    """ https://stackoverflow.com/questions/16571150/how-to-capture-stdout-output-from-a-python-function-call
+    # def test_project_number_to_project_key_finds_project_key_from_project_number
+    # def test_get_projects_matching_only_match_projects_containing_word
+    # def test_get_projects_matching_only_match_projects_containing_words
+    # def test_add_project_adds_project_to_top_of_the_list
+    # def test_delete_project
+    # def test_get_opts_matches_-d
+    # def test_get_opts_matches_--delte
+    # def test_get_opts_matches_-a
+    # def test_get_opts_matches_--add
+    # def test_get_opts_matches_-b
+    # def test_get_opts_matches_--bump
+    # def test_get_opts_matches_GOTO
+
+    # test:
+    # find project
+    # project exists
+    # number to key
+    # key to number
+
+
+
+
